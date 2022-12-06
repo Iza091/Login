4fbbffd33796da32ee9869b101cd44803090f127
Eduardo Isaías Villegas Ortiz <44987358+Eddo091@users.noreply.github.com>
2022-12-06 10:56:32 -0600

    Subir archivos

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+$nombre=$_POST["nombre"];
+$email=$_POST["email"];
+echo "Bienvenido: " .  $nombre . ", <br/>
+Tu correo: "  .$email . ".";
+?>
---
+++ login.php
@@ -0,0 +1,7 @@
+<form  
+action="index.php">
+Inicia Sesión<br/>
+Nombre: <input type="text" name="nombre"/> <br/>
+email: <input type="text" name="email"/> <br/>
+<input type="submit" value="Iniciar"/>
+</form>
